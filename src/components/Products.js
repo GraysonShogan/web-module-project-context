39917af1c453aa3e503717588dbbd1c59edbee99
Grayson Shogan <graysontoddshogan@gmail.com>
2023-05-04 17:54:26 -0700

    wip

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
 
-// Components
-import Product from './Product';
+const Products = () => {
+  const { products, addItem } = useContext(ProductContext);
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <div className="product-card" key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.price}</p>
+          <button onClick={() => addItem(product)}>Add to cart</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Products;
